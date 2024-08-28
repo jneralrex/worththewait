de5a57a598d3d738b14ba2fa12bccda5e14c1f0b
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-08-28 02:09:35 +0100

    Added timer to cancle snack bar and adjusted invite note width

--- src/components/Snackbar.jsx
+++ src/components/Snackbar.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import '../assets/styles/Snackbar.css'
 import { MdOutlineClear } from "react-icons/md";
 
 const Snackbar = ({ message, type, onClose }) => {
+  const [sanckbarcountdown, setSnackBarCountdown] = useState(3);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSnackBarCountdown(prev => prev - 1);
+    }, 2000);
+
+    if (sanckbarcountdown === 0) {
+      onClose();
+    }
+
+    return () => clearInterval(timer);
+  }, [sanckbarcountdown, onClose]);
     return (
       <div className={`snackbar ${type}`}>
         {message}
-        <button onClick={onClose}><MdOutlineClear/></button>
+        <MdOutlineClear size={40} onClick={onClose} style={{color:'black'}}/>
       </div>
     );
   };
